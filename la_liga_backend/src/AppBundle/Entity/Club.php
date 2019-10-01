20b393cc2035590cd3e8e643a3280ecf72a153d1
Mick <mickjtj@hotmail.com>
2019-10-01 22:44:56 +0200

    creadas las entidades

---
+++ la_liga_backend/src/AppBundle/Entity/Club.php
@@ -0,0 +1,140 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\ArrayCollection;
+
+/**
+ * Club
+ *
+ * @ORM\Table(name="club")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\ClubRepository")
+ */
+class Club
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\OneToMany(targetEntity="Jugador", mappedBy="club")
+     */
+    private $jugadores;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nombre", type="string", length=50)
+     */
+    private $nombre;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="estadio", type="string", length=50)
+     */
+    private $estadio;
+
+    public function __construct()
+    {
+        $this->jugadores = new ArrayCollection();
+    }
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nombre
+     *
+     * @param string $nombre
+     *
+     * @return Club
+     */
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    /**
+     * Get nombre
+     *
+     * @return string
+     */
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    /**
+     * Set estadio
+     *
+     * @param string $estadio
+     *
+     * @return Club
+     */
+    public function setEstadio($estadio)
+    {
+        $this->estadio = $estadio;
+
+        return $this;
+    }
+
+    /**
+     * Get estadio
+     *
+     * @return string
+     */
+    public function getEstadio()
+    {
+        return $this->estadio;
+    }
+
+    /**
+     * Add jugadore
+     *
+     * @param \AppBundle\Entity\Jugador $jugador
+     *
+     * @return Club
+     */
+    public function addJugador(\AppBundle\Entity\Jugador $jugador)
+    {
+        $this->jugadores[] = $jugador;
+
+        return $this;
+    }
+
+    /**
+     * Remove jugadore
+     *
+     * @param \AppBundle\Entity\Jugador $jugador
+     */
+    public function removeJugador(\AppBundle\Entity\Jugador $jugador)
+    {
+        $this->jugadores->removeElement($jugador);
+    }
+
+    /**
+     * Get jugadores
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getJugadores()
+    {
+        return $this->jugadores;
+    }
+}
---
+++ la_liga_backend/src/AppBundle/Entity/Jugador.php
@@ -0,0 +1,157 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Jugador
+ *
+ * @ORM\Table(name="jugador")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\JugadorRepository")
+ */
+class Jugador
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Club", inversedBy="jugadores")
+     * @ORM\JoinColumn(name="club_id", referencedColumnName="id")
+     */
+    private $club;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nombre", type="string", length=50)
+     */
+    private $nombre;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="dorsal", type="integer")
+     */
+    private $dorsal;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="posicion", type="string", length=10)
+     */
+    private $posicion;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nombre
+     *
+     * @param string $nombre
+     *
+     * @return Jugador
+     */
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    /**
+     * Get nombre
+     *
+     * @return string
+     */
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    /**
+     * Set dorsal
+     *
+     * @param integer $dorsal
+     *
+     * @return Jugador
+     */
+    public function setDorsal($dorsal)
+    {
+        $this->dorsal = $dorsal;
+
+        return $this;
+    }
+
+    /**
+     * Get dorsal
+     *
+     * @return int
+     */
+    public function getDorsal()
+    {
+        return $this->dorsal;
+    }
+
+    /**
+     * Set posicion
+     *
+     * @param string $posicion
+     *
+     * @return Jugador
+     */
+    public function setPosicion($posicion)
+    {
+        $this->posicion = $posicion;
+
+        return $this;
+    }
+
+    /**
+     * Get posicion
+     *
+     * @return string
+     */
+    public function getPosicion()
+    {
+        return $this->posicion;
+    }
+
+    /**
+     * Set club
+     *
+     * @param \AppBundle\Entity\Club $club
+     *
+     * @return Jugador
+     */
+    public function setClub(\AppBundle\Entity\Club $club = null)
+    {
+        $this->club = $club;
+
+        return $this;
+    }
+
+    /**
+     * Get club
+     *
+     * @return \AppBundle\Entity\Club
+     */
+    public function getClub()
+    {
+        return $this->club;
+    }
+}
